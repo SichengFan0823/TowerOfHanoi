716b890a3acc2618acb105396948d5a0afe07031
Sicheng Fan <145726565+SichengFan0823@users.noreply.github.com>
2023-09-23 10:15:16 -0400

    Add files via upload
    an enum representing the possible positions of a tower in the Tower of Hanoi game.

---
+++ Position.java
@@ -0,0 +1,19 @@
+// Virginia Tech Honor Code Pledge:
+//f
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of those who do.
+// Sicheng Fan
+
+package towerofhanoi;
+
+/**
+ *
+ * An enum representing the possible positions of a tower in the Tower of Hanoi
+ * game.
+ * 
+ * @author
+ */
+public enum Position {
+
+	LEFT, MIDDLE, RIGHT, DEFAULT
+}
