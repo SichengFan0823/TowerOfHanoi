3f9b9624757440805a8ac0c98ed3543c9e4f55ff
Sicheng Fan <145726565+SichengFan0823@users.noreply.github.com>
2023-09-23 10:17:56 -0400

    Add files via upload
    tests the ` Tower` class.

---
+++ TowerTest.java
@@ -0,0 +1,21 @@
+package towerofhanoi;
+
+import student.TestCase;
+
+public class TowerTest extends TestCase {
+
+	public void setUp()
+	 {
+		
+	 }
+	
+	public void testPosition() {
+		
+	}
+	
+	public void testPush() {
+		
+	}
+
+
+}
